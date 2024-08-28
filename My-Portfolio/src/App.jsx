2c9e39d2a8ff9277e092c3dd9138339185af6d9d
onkarpatole <onkarpatole28@gmail.com>
2024-08-28 18:04:41 -0400

    Created the components folder and Navbar files

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 export const App = () => {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   )
 }
+export default App
