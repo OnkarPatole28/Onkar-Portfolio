b77c752690156374de27f38dbe7faf2b25faa0e8
onkarpatole <onkarpatole28@gmail.com>
2024-08-30 22:52:29 -0400

    Updated Navbar.jsx

--- My-Portfolio/src/Components/Navbar/Navbar.jsx
+++ My-Portfolio/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,15 @@ import './Navbar.css'
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src="" alt="" />
+        <img src="{logo}" alt="" />
+        <ul className="nav-menu">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect With Me</div>
 
     </div>
   )
