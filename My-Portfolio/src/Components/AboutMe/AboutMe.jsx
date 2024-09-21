6dc0db5ba6123ab63b5c3335da3e4dda7b512c14
onkarpatole <onkarpatole28@gmail.com>
2024-09-21 14:19:49 -0400

    fixed the AboutMe Section

--- My-Portfolio/src/Components/AboutMe/AboutMe.jsx
+++ My-Portfolio/src/Components/AboutMe/AboutMe.jsx
@@ -1,50 +1,53 @@
 import React from 'react'
-import './AboutMe.css'
+import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import profile_img from '../../assets/about_profile.svg'
 
+
 const About = () => {
-    return (
-      <div id='about' className='about'>
-        <div className="about-title">
-          <h1>About me</h1>
-          <img src={theme_pattern} alt="" />
+  return (
+    <div id='about' className='about'>
+      <div className="title-box">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+            <img src={profile_img} alt="" />
         </div>
-        <div className="about-sections">
-          <div className="about-left">
-              <img src={profile_img} alt="" />
-          </div>
-          <div className="about-right">
-              <div className="about-para">
-                  <p>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
-                  <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
-              </div>
-              <div className="about-skills">
-                  <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}} /></div>
-                  <div className="about-skill"><p>React JS</p><hr style={{width:"70%"}} /></div>
-                  <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}} /></div>
-                  <div className="about-skill"><p>Next JS</p><hr style={{width:"50%"}} /></div>
-              </div>
-          </div>
-        </div>
-        <div className="about-achievements">
-          <div className="about-achievement">
-              <h1>3+</h1>
-              <p>YEARS OF EXPERIENCE</p>
-          </div>
-          <hr />
-          <div className="about-achievement">
-              <h1>10+</h1>
-              <p>PROJECTS COMPLETED</p>
-          </div>
-          <hr />
-          <div className="about-achievement">
-              <h1>3x</h1>
-              <p>AWS Certified</p>
-          </div>
+        <div className="about-right">
+            <div className="about-para">
+                <p>I am Onkar Patole, a Computer Science graduate student at Binghamton University (SUNY) with over 3 years of professional experience as a Software Developer and Data Engineer.</p>
+                <p>Currently, I am working as a Cloud Site Reliability Engineer Intern at Michigan Health Information Network Shared Services (MiHIN).</p>
+            </div>
+            <div className="about-skills">
+                <div className="about-skill"><p>AWS</p><hr style={{width:"80%"}} /></div>
+                <div className="about-skill"><p>React JS, Node JS & JavaScript</p><hr style={{width:"70%"}} /></div>
+                <div className="about-skill"><p>Docker, Kuberentes, Terraform & Kafka</p><hr style={{width:"60%"}} /></div>
+                <div className="about-skill"><p>Python, Java, SQL</p><hr style={{width:"90%"}} /></div>
+            </div>
         </div>
       </div>
-    )
-  }
-  
-  export default About
+      <div className="about-achievements">
+        <div className="about-achievement">
+            <h1>3+</h1>
+            <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <a href="https://www.credly.com/users/onkar-patole.9a0b2ab9" target="_blank" rel="noopener noreferrer" className="achievement-link">
+            <h1>3x</h1>
+            <p>AWS CERTIFIED</p>
+          </a>
+        </div>
+        <hr />
+        <div className="about-achievement">
+            <h1>5+</h1>
+            <p>PROJECTS COMPLETED</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
