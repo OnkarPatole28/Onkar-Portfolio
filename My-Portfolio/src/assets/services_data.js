fd5142a6e9cfeda4cda00cb721b160837342a9ca
onkarpatole <onkarpatole28@gmail.com>
2025-04-12 11:47:46 -0400

    .

--- My-Portfolio/src/assets/services_data.js
+++ My-Portfolio/src/assets/services_data.js
@@ -2,12 +2,12 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Michigan Health Information Network Shared Services (MiHIN)",
-        s_desc:"Currently working as Cloud DevOps Engineer Intern. Collaborating with development and QA teams to implement cloud solutions using Terraform, Kubernetes, Docker and Serverless Frameworks for healthcare data."
+        s_desc:"Currently working as Cloud DevOps Engineer Intern. Collaborating with Development and DevOps teams to implement cloud solutions using Terraform, Kubernetes, Docker and Serverless Frameworks for healthcare data."
     },
     {
         s_no:"02",
         s_name:"LTIMindtree",
-        s_desc:"Worked as a Software Engineer. Contributed in developement and execution of an ETL data pipeline on AWS with a team of developers, transforming and modeling data, optimizing storage, and managing the entire process."
+        s_desc:"Worked as a Software Engineer, enhancing Jenkins CI/CD for JavaScript apps by reducing deployment time, building API testing with Postman, integrating AWS Secrets Manager, and automating Kubernetes updates using Argo CD and Helm."
     },
     {
         s_no:"03",
